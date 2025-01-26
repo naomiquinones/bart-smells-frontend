0a966bcc6dc9ab019ed51ee157ac13520a00eea0
Naomi Quinones <naomiquinones@gmail.com>
2025-01-26 02:46:40 -0800

    Rename files

--- src/components/CheckLogin.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-
-const CheckLogin = ({ isLoggedIn, ...props}) => isLoggedIn ? <Route {...props} /> :
-  <Redirect to="/login" />;
-
-
-export default CheckLogin;
--- src/components/Option.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Option.css';
-
-const Option = ({name, number}) => (
-  <option value={number}>{name}</option>
-);
-
-export default Option;
